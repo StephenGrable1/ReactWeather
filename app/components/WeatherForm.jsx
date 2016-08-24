ee7172c0a853e74969fc700fe6a7bb009457b623
StephenGrable1 <stephengrable@gmail.com>
2016-08-23 21:16:37 -0400

    Switch to SCSS

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -14,7 +14,7 @@ var WeatherForm = React.createClass ({
   render: function(){
     return (
       <form onSubmit={this.onFormSubmit}>
-        <input type="search" ref="location" placeholder="Search Weather By City"/>
+        <input type="search" ref="location" placeholder="Search weather by city"/>
         <button className="button expanded hollow">Get Weather</button>
       </form>
     );
